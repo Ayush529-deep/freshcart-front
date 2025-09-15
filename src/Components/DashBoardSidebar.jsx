d58c3a76b8b379c7a8b5329d1bb16e4c8c86ee4e
Ayush529-deep <ayush.abhatt@gmail.com>
2025-09-15 13:12:05 +0530

    DashBoardSidebar.jsx

--- src/Components/DashBoardSidebar.jsx
+++ src/Components/DashBoardSidebar.jsx
@@ -15,12 +15,12 @@ function DashBoardSidebar(){
   return (
     <div className="w-64 bg-white shadow h-screen p-4 flex flex-col gap-2 border-r dashboardnav">
       <h2 className="text-2xl font-bold flex items-center gap-2 text-green-700 mb-6">
-        <span className="text-3xl">🛒</span> FreshCart
+        <span className="text-3xl">🛒</span> <Link to="/home">FreshCart</Link>   
       </h2>
 
       <div>
         <button className="flex items-center w-full gap-3 px-4 py-2 rounded bg-green-100 text-green-700 font-semibold">
-          <FaHome /> Dashboard
+          <FaHome /> <Link to="/dashboard">Dashboard</Link> 
         </button>
       </div>
 
